2c7d453acf1aa0ebec6c1c1e5a2571d183c298cf
ayeshapatel03 <ayeshap3@uw.edu>
2023-02-27 17:50:09 -0800

    change trends

--- chart_2.R
+++ chart_2.R
@@ -3,38 +3,29 @@ library(stringr)
 library(ggplot2)
 
 all_data <- read.csv("C://Users//creat//OneDrive - UW//winter 2023//INFO 201//Checkouts_by_Title.csv", stringsAsFactors = FALSE)
-by_series <- all_data %>%
+by_book <- all_data %>%
   filter(Creator == "Sarah J. Maas" | Creator == "Maas, Sarah J.")
 
-by_series$Title[str_detect(tolower(by_series$Title), "a court of")] <- "A Court of Thorns and Roses Series"
+by_book$Title[str_detect(tolower(by_book$Title), "a court of frost and starlight")] <- "A Court of Frost and Starlight"
+by_book$Title[str_detect(tolower(by_book$Title), "a court of mist and fury")] <- "A Court of Mist and Fury"
+by_book$Title[str_detect(tolower(by_book$Title), "a court of silver flames")] <- "A Court of Silver Flames"
+by_book$Title[str_detect(tolower(by_book$Title), "a court of wings and ruin")] <- "A Court of Wings and Ruin"
+by_book$Title[str_detect(tolower(by_book$Title), "a court of thorns and roses")] <- "A Court of Thorns and Roses"
 
-by_series$Title[str_detect(tolower(by_series$Title), "throne of glass")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "crown of midnight")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "empire of storms")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "heir of fire")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "kingdom of ash")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "queen of shadows")] <- "Throne of Glass Series"
-by_series$Title[str_detect(tolower(by_series$Title), "tower of dawn")] <- "Throne of Glass Series"
+by_book <- by_book %>%
+  mutate(Series = NULL)
 
-by_series$Title[str_detect(tolower(by_series$Title), "catwoman")] <- "Catwoman: Soulstealer"
+by_book$Series[str_detect(tolower(by_book$Title), "a court of")] <- "A Court of Thorns and Roses Series"
 
-by_series$Title[str_detect(tolower(by_series$Title), "crescent city")] <- "Crescent City Series"
-
-by_series$Title[str_detect(tolower(by_series$Title), "house of earth and blood")] <- "Crescent City Series"
-by_series$Title[str_detect(tolower(by_series$Title), "house of sky and breath")] <- "Crescent City Series"
-
-Series <- c("A Court of Thorns and Roses Series", "Crescent City Series", "Catwoman: Soulstealer", "Throne of Glass Series")
-
-by_series <- by_series %>%
-  filter(Title == Series, na.rm = TRUE) %>%
+by_book <- by_book %>%
+  filter(Series == "A Court of Thorns and Roses Series") %>%
   mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"), na.rm = TRUE) %>%
   mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
   group_by(Title, date) %>%
   summarize(sum = sum(Checkouts))
 
-ggplot(data = by_series) +
+ggplot(data = by_book) +
   geom_line(mapping = aes(x = date , y = sum, color = Title)) + 
   labs(title = "Checkouts over Time by Book Series", x = "Date", y = "Checkouts") +
   scale_color_brewer(palette = "Dark2") +
   theme_bw()
-
